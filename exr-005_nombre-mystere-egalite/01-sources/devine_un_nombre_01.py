748b857d563695ed0dbdc735fd410602b97aa10c
kanzalakramsi <63557030+kanzalakramsi@users.noreply.github.com>
2020-04-13 13:53:54 +0200

    Update devine_un_nombre_01.py

--- exr-005_nombre-mystere-egalite/01-sources/devine_un_nombre_01.py
+++ exr-005_nombre-mystere-egalite/01-sources/devine_un_nombre_01.py
@@ -2,3 +2,5 @@ nombre_mystere = 7
 
 # Demander à l'utilisateur de deviner le nombre.
 # Afficher si le nombre entré par l'utilisateur est égal au nombre mystère.
+n_m=input("entrez le nombre mystère: ")
+print(n_m==nombre_mystere)
